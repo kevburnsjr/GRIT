389ffefe2d5f921b68b898cc2d6a9fcf8052b914
KevBurnsJr <kevburnsjr@gmail.com>
2010-09-27 14:18:08 -0700

    Adding DOC_ROOT var.

--- grit-create.sh
+++ grit-create.sh
@@ -4,6 +4,7 @@ source config.sh
 
 REPO_DIR=$GRIT_REPO_DIR/$1.git
 WWW_DIR=$GRIT_WWW_DIR/$1
+DOC_ROOT=$WWW_DIR/dev
 
 if [ -z $1 ]; then
     echo "Please specify a repository."
@@ -24,9 +25,8 @@ echo "Repo Created."
 
 # =========== Create Hooks ===========
 
-rm hooks/post-receive
 cp $GRIT_SCRIPT_DIR/tpl/post-receive hooks/post-receive
-sed "s/DOC_ROOT/$WWW_DIR\/dev/" hooks/post-receive -i
+sed "s/DOC_ROOT/$DOC_ROOT/" hooks/post-receive -i
 chmod 755 hooks/post-receive
 
 echo "Repo Hooks Created."
@@ -34,7 +34,7 @@ echo "Repo Hooks Created."
 # =========== Create Document Root ===========
 
 cp $GRIT_SCRIPT_DIR/pull.sh $WWW_DIR
-sed "s/DOC_ROOT/$WWW_DIR\/dev/" $WWW_DIR/pull.sh -i
+sed "s/DOC_ROOT/$DOC_ROOT/" $WWW_DIR/pull.sh -i
 
 mkdir $WWW_DIR/dev
 cd $WWW_DIR/dev
@@ -47,7 +47,7 @@ echo "Document Root Created."
 
 cp $GRIT_SCRIPT_DIR/tpl/vhost.conf $GRIT_VHOST_DIR/$1.conf
 sed "s/REPO_NAME/$1/" $GRIT_VHOST_DIR/$1.conf -i
-sed "s/DOC_ROOT/$WWW_DIR\/dev/" $GRIT_VHOST_DIR/$1.conf -i
+sed "s/DOC_ROOT/$DOC_ROOT/" $GRIT_VHOST_DIR/$1.conf -i
 sed "s/HOST/$GRIT_HOST/" $GRIT_VHOST_DIR/$1.conf -i
 
 sudo /etc/init.d/httpd reload

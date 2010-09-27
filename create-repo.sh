5b6169ecf9f17f21652794d65e571f6dc442f96b
KevBurnsJr <kevburnsjr@gmail.com>
2010-09-27 03:22:46 -0700

    shuffling files

--- create-repo.sh
+++ create-repo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-GIT_DIR=~/$1.git
-DOC_ROOT=/var/www/$1
+GIT_DIR=$GRIT_REPO_DIR/$1.git
+DOC_ROOT=$GRIT_WWW_DIR/$1
 
 if [ -z $1 ]; then
     echo "Please specify a repository."
     exit 1
 fi
-if [ -d $GIT_DIR ]; then
+if [ -d $GRIT_REPO_DIR/$1.git ]; then
     echo "Repository already exists."
     exit 1
 fi
@@ -19,22 +19,22 @@ git --bare init
 echo "Repo Created."
 
 rm hooks/post-receive
-cp /var/git/scripts/post-receive.tpl hooks/post-receive
+cp $GRIT_SCRIPT_DIR/post-receive.tpl hooks/post-receive
 sed "s/REPO_NAME/$1/" hooks/post-receive -i
 chmod 755 hooks/post-receive
 
 echo "Repo Hooks Created."
 
-cp -R /var/www/tpl $DOC_ROOT
-sed "s/REPO_NAME/$1/" $DOC_ROOT/scripts/pull.sh -i
+cp -R $GRIT_SCRIPT_DIR/pull.sh $DOC_ROOT
+sed "s/REPO_NAME/$1/" $DOC_ROOT/pull.sh -i
 mkdir $DOC_ROOT/dev
 cd $DOC_ROOT/dev
 git init
-git remote add origin /home/git/$1.git
+git remote add origin $GRIT_REPO_DIR/$1.git
 
 echo "Document Root Created."
 
-VHOST_DIR=/var/www/vhosts
+VHOST_DIR=GRIT_VHOST_DIR
 cp $VHOST_DIR/vhost.tpl $VHOST_DIR/$1.conf
 sed "s/REPO_NAME/$1/" $VHOST_DIR/$1.conf -i
 
---
+++ tpl/pull.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cd /var/www/REPO_NAME/dev
+git pull origin master
+exit

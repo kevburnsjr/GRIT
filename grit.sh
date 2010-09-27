89f14f222aebf46466791ffa8d25c3642a54a1ae
KevBurnsJr <kevburnsjr@gmail.com>
2010-09-27 06:24:53 -0700

    fixing logic in bash.sh

--- grit.sh
+++ grit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $1 = "create" or $1 = "c" ]; then
+if [ $1 = "create" -o $1 = "c" ]; then
     source grit-create.sh $2
     exit 1
 fi
-if [ $1 = "remove" or $1 = "rm" ]; then
+if [ $1 = "remove" -o $1 = "rm" ]; then
     source grit-remove.sh $2
     exit 1
 fi
